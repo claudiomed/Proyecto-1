db93d11f0840d61c1d0cd23a0fe52c3deb3883bf
claudiomed <40347636+claudiomed@users.noreply.github.com>
2020-06-21 00:23:58 -0600

    Subiendo Proyecto
    Subiendo Proyecto

---
+++ ToDoList/DAL/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL
+{
+    public class Conexion
+    {
+
+        public SqlCommand abrirConexion()
+        {
+            try
+            {
+                var connectionString = Properties.Settings.Default.Conexion.ToString();
+                SqlConnection cn = new SqlConnection(connectionString);
+                cn.Open();
+                SqlCommand objCmd = new SqlCommand();
+                return objCmd;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        
+    }
+}
